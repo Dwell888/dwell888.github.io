c84e44ff3887ca6bc97723cd87dbe41e9a2e5cf4
Dwell888 <unwit6@gmail.com>
2026-04-11 00:42:47 -0400

    1.19
    removal

--- global.js
+++
@@ -1,7 +0,0 @@
-document.title = "FlightReacts";
-
-let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-link.type = 'image/png';
-link.rel = 'icon';
-link.href = '/favicon.png';
-document.head.appendChild(link);
